9224ce6f08ad4543fb4d93a1292581c0fcfe89f4
Diana <edvi.diana@gmail.com>
2026-03-03 21:28:59 -0500

    Brush center calculated

--- src/Terraformer.cpp
+++ src/Terraformer.cpp
@@ -7,10 +7,30 @@
 bool Terraformer::applyBrush(const Eigen::Vector3f& ray_origin, const Eigen::Vector3f& ray_dir) {
     igl::Hit<double> hit;
 
-    if (igl::ray_mesh_intersect(ray_origin, ray_dir, terrain.V, terrain.F, hit)) {
-        Eigen::Vector3f hitPoint = ray_origin + hit.t * ray_dir;
-        std::cout << "Hit point: " << hitPoint.transpose() << std::endl;
-        // TODO: apply brush influence using 'brush.radius' and 'hitPoint'
+    if (igl::ray_mesh_intersect(ray_origin.cast<double>(), ray_dir.cast<double>(), terrain.V, terrain.F, hit)) {
+        
+        float t_f = static_cast<float>(hit.t);
+        Eigen::Vector3f hitPoint = ray_origin + t_f * ray_dir;
+        
+        // Get indices from face matrix
+        int i0 = terrain.F(hit.id, 0);
+        int i1 = terrain.F(hit.id, 1);
+        int i2 = terrain.F(hit.id, 2);
+
+        // Look up vertices
+        Eigen::Vector3f v0 = terrain.V.row(i0).cast<float>();
+        Eigen::Vector3f v1 = terrain.V.row(i1).cast<float>();
+        Eigen::Vector3f v2 = terrain.V.row(i2).cast<float>();
+        
+        // Use static_cast for barycentric coordinates
+        float u_f = static_cast<float>(hit.u);
+        float v_f = static_cast<float>(hit.v);
+        float w_f = 1.0f - u_f - v_f;
+        
+        // Calculate center
+        Eigen::Vector3f brushCenter = (v0 * w_f) + (v1 * u_f) + (v2 * v_f);
+        
+        std::cout << "Brush center: " << brushCenter.transpose() << std::endl;
         return true;
     }
     return false;
